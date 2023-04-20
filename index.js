483da1c8ac433e03755264c47e7070c50e56c515
tanyak1601 <tanyakk1601@gmail.com>
2023-04-20 13:59:34 +0300

    fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const sharp = require('sharp');
 
 async function check() {
-    const file = process.argv[0]
+    const file = process.argv[2]
     console.log(file);
     try {
-        sharp('./4bd8be08-3b70-40ab-9945-8184cf11d20d.jpeg').toBuffer()
+        sharp(file).toBuffer()
     } catch (error) {
         console.log(error);
     }
